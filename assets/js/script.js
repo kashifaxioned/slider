76a7df61810f8fa48aff22ca7a387bd0ed2e1a73
kashifaxioned <kashifa@axioned.com>
2022-04-11 03:56:24 +0530

    JS functionaity added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,32 @@
 
 */
 
+let slides = $(".slides")
+$(".left-btn").click(backward)
+$(".right-btn").click(forward)
 
+let counter = 0
 
+function backward() {
+  counter--;
+  slides.map((index, slide) => $(slide).addClass("backward"))
+  showSlide()
+}
 
+function forward() {
+  counter++;
+  slides.map((index, slide) => $(slide).addClass("forward"))
+  showSlide()
+}
+
+function showSlide() {
+  if(counter < 0) counter = slides.length - 1
+  else if(counter >= slides.length) counter = 0 
+  slides.map((index, slide) => {
+    if(index === counter) $(slide).addClass("show")
+    else $(slide).removeClass("show")
+  })
+}
 
 
 
